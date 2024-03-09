b4bc05758359ab21e094ded377f652a8e97eb398
hulkarv <ernazarovahulkar@gmail.com>
2024-03-09 12:33:16 +0500

    Initial commit

---
+++ EventsManager/EventsManager/Data/EventManagerDbContext.cs
@@ -0,0 +1,12 @@
+﻿using EventsManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventsManager.Data
+{
+    public class EventManagerDbContext : DbContext
+    {
+        public EventManagerDbContext(DbContextOptions<EventManagerDbContext> options) : base(options) { }
+
+        public DbSet<Event> events { get; set; }
+    }
+}
---
+++ EventsManager/EventsManager/Models/Event.cs
@@ -0,0 +1,12 @@
+﻿namespace EventsManager.Models
+{
+    public class Event
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime Time { get; set; }
+        public string Organizer { get; set; }
+    }
+}
