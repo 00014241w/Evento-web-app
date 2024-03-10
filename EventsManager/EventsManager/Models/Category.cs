6456e5fa34cc8523179f8496d761e616e2d55754
hulkarv <ernazarovahulkar@gmail.com>
2024-03-10 14:13:29 +0500

    Added validation
    Added validation to required field in both events and categories models

--- EventsManager/EventsManager/Models/Category.cs
+++ EventsManager/EventsManager/Models/Category.cs
@@ -1,8 +1,14 @@
-﻿namespace EventsManager.Models
+﻿using System;
+using EventsManager.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EventsManager.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Category is required!")]
         public string CategoryName { get; set; }
     }
 }
--- EventsManager/EventsManager/Models/Event.cs
+++ EventsManager/EventsManager/Models/Event.cs
@@ -1,4 +1,6 @@
-﻿using EventsManager.Data;
+﻿using System;
+using EventsManager.Data;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EventsManager.Models
@@ -6,9 +8,13 @@ namespace EventsManager.Models
     public class Event
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "Title is required!")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Description is required!")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Location is required!")]
         public string Location { get; set; }
+        [Required(ErrorMessage = "TIme is required!")]
         public DateTime Time { get; set; }
         public string Organizer { get; set; }
         public int? CategoryId { get; set; }
